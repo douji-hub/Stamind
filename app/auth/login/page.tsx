f2a21295e97995e6fc57a219a46e6caba439be8d
eric <26416387.re@gmail.com>
2024-10-23 15:26:33 +0800

    1000 feat:登入畫面切版

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+import LogInForm from "@/components/LogIn/LogInForm";
+
+const page = () => {
+  return (
+    <div>
+      <div className="h-[31.25rem] w-[37.5rem]">
+        <div className="h-[8.75rem] w-full">
+          <Image
+            className="h-[2.125rem] w-[13.75rem] mx-auto text-center"
+            alt="logo"
+            width={100}
+            height={100}
+            src="/LoginLogo/logo1.svg"
+          />
+          <div className="mt-[3rem] text-center text-[3rem] font-bold">
+            Unlock your workspace
+          </div>
+        </div>
+        <LogInForm />
+      </div>
+    </div>
+  );
+};
+
+export default page;
