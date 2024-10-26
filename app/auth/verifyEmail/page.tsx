97446601252a3623edb63cbe257e19af86851494
zjimf <jmjmjim1101@gmail.com>
2024-10-26 23:22:33 +0800

    refactor: remove parameter

--- app/auth/verifyEmail/page.tsx
+++ app/auth/verifyEmail/page.tsx
@@ -17,7 +17,7 @@ const page: React.FC<IVerifyEmail> = () => {
         <br /> Please check your email and click the verification link to
         continue.
       </div>
-      <Reciprocal time={10} />
+      <Reciprocal />
       <div className="flex flex-col justify-start bg-uiSpecificColor-stamind-bg-command mt-[5rem] w-[42.625rem]">
         <div className="px-[0.625rem] py-[0.5rem] text-[0.6875rem] text-lightgrey-stamind-lightgrey-200">
           <span className="font-bold">Didn't receive the email?</span>
