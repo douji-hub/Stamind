76de8244bea4636c9a3db858672cd1f5cb286b91
zjimf <jmjmjim1101@gmail.com>
2024-11-09 09:53:17 +0800

    refactor: remove passwordField, change to public inputComponent

--- app/auth/resetPassword/page.tsx
+++ app/auth/resetPassword/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+<<<<<<< HEAD
 import React from 'react'
 import { useForm } from '@/hooks/auth/useForm'
 import InputComponent from '@/components/form/input/InputComponent'
@@ -17,6 +18,19 @@ const Page: React.FC = () => {
   const { password, confirmPassword } = formValues
   const { setPassword, setConfirmPassword } = setFormValues
   const { setHasTyped } = typingStatus
+=======
+import React from "react";
+import { useForm } from "./hooks/useForm";
+import InputComponent from "@/components/form/input/InputComponent";
+
+const Page: React.FC = () => {
+  const { formValues, setFormValues, typingStatus, errors, isFormValid } =
+    useForm();
+
+  const { password, confirmPassword } = formValues;
+  const { setPassword, setConfirmPassword } = setFormValues;
+  const { hasTyped, setHasTyped } = typingStatus;
+>>>>>>> fa9f0fd (refactor: remove passwordField, change to public inputComponent)
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value)
@@ -40,6 +54,7 @@ const Page: React.FC = () => {
       </div>
       <div className="flex flex-col justify-center items-center mt-[17rem] gap-4">
         <InputComponent
+<<<<<<< HEAD
           label="Password"
           type="password"
           placeholder="Enter your password"
@@ -59,6 +74,50 @@ const Page: React.FC = () => {
           hasError={confirmPasswordError.hasError}
           errorMessage={confirmPasswordError.errorMessages}
           boxClass="w-[22rem]"
+=======
+          label={"Password"}
+          type={"password"}
+          placeholder={"Enter your password"}
+          value={password}
+          onChange={handlePasswordChange}
+          hasError={
+            hasTyped.password &&
+            (!errors.length ||
+              !errors.uppercase ||
+              !errors.lowercase ||
+              !errors.number ||
+              !errors.specialChar)
+          }
+          errorMessage={
+            hasTyped.password
+              ? [
+                  !errors.length &&
+                    "Your password must be at least 8 characters long.",
+                  !errors.uppercase && "It must include uppercase letters.",
+                  !errors.lowercase && "It must include lowercase letters.",
+                  !errors.number && "It must include numbers.",
+                  !errors.specialChar &&
+                    "It must include special characters (e.g., !, @, #, etc.).",
+                ].filter(Boolean)
+              : undefined
+          }
+          boxClass={"w-[22rem]"}
+        />
+
+        <InputComponent
+          label={"Confirm Password"}
+          type={"confirmPassword"}
+          placeholder={"Enter your password again"}
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+          hasError={hasTyped.confirmPassword && !errors.match}
+          errorMessage={
+            hasTyped.confirmPassword && !errors.match
+              ? ["Passwords do not match. Please try again."]
+              : undefined
+          }
+          boxClass={"w-[22rem]"}
+>>>>>>> fa9f0fd (refactor: remove passwordField, change to public inputComponent)
         />
 
         <button
