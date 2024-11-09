53bf33c3a21312054e23af20ec50a03a85ffbf00
eric <26416387.re@gmail.com>
2024-11-09 19:53:02 +0800

    引入組建

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Sidebar from "@/components/sidebar/sidebar.tsx";
+
 export default function Home() {
-  return <div>homePage</div>;
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
 }
