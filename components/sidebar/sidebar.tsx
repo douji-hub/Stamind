c9812f4fdc921b8dd4d4c8113f7a298b97429b17
eric <26416387.re@gmail.com>
2024-11-15 10:58:41 +0800

    解typescript錯誤

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
               />
               <div className="ms-[0.5rem] text-[0.8rem]">My Workspace</div>
             </div>
-            <Image src="/images/Sidebar/sidebar.svg" width={14} height={14} />
+            <Image src="/images/Sidebar/sidebar.svg" width={14} height={14} alt="sidebar"/>
           </div>
           <div className="flex items-center  justify-between mt-[0.75rem] ">
             <input
@@ -30,30 +30,30 @@ const Sidebar = () => {
               placeholder="Search..."
               className="w-[9.5rem] h-[1.3rem] bg-black-stamind-black-850 border border-transparent focus:border-primary-stamind-blue-800 focus:outline-none placeholder:text-[#9E9E9E] placeholder:text-[15px]"
             />
-            <Image src="/images/Sidebar/search-1.svg" width={16} height={16} />
+            <Image src="/images/Sidebar/search-1.svg" width={16} height={16}  alt="search"/>
           </div>
         </div>
 
         <div className="flex items-center justify-between">
           <div className="w-[5.75rem] h-[4.5rem] rounded-[0.25rem] border border-[#666666] flex flex-col justify-center items-center">
-            <Image src="/images/Sidebar/note.svg" width={16} height={16} />
+            <Image src="/images/Sidebar/note.svg" width={16} height={16} alt="note" />
             <div className="text-[13px]">Note</div>
           </div>
           <div className="w-[5.75rem] h-[4.5rem] rounded-[0.25rem] border border-[#666666] flex flex-col justify-center items-center">
-            <Image src="/images/Sidebar/code.svg" width={16} height={16} />
+            <Image src="/images/Sidebar/code.svg" width={16} height={16} alt="code" />
             <div className="text-[13px]">Code</div>
           </div>
         </div>
         <div className="mt-[1rem]">
           <div className="flex items-center justify-between px-[0.5rem] py-[0.25rem]">
             <div className="text-[12px] text-[#9E9E9E] ">Personal</div>
-            <Image src="/images/Sidebar/folder-add.svg" width={16} height={16}></Image>
+            <Image src="/images/Sidebar/folder-add.svg" width={16} height={16} alt="folder"></Image>
           </div>
           <div className="w-[12rem] h-[16rem] flex flex-col items-center justify-center ">
             <div>
               <div className="text-center text-[15px] font-normal leading-[22px] text-[#4D4F69]">Add a</div>
               <div className="text-[#4D4F69]">Note or Code</div>
-              <Image src="/images/Sidebar/note.svg" className="mx-auto" width={48} height={48} />
+              <Image src="/images/Sidebar/note.svg" className="mx-auto" width={48} height={48} alt="note" />
             </div>
           </div>
           {/* <div className="flex items-center space-x-1 px-[0.5rem] py-[0.25rem]">
@@ -65,9 +65,9 @@ const Sidebar = () => {
       </div>
 
       <div className="flex items-center justify-between px-[1.4rem] pb-[1.4rem]">
-        <Image src="/images/Sidebar/question.svg" width={16} height={16} />
-        <Image src="/images/Sidebar/three-dots.svg" width={16} height={16} />
-        <Image src="/images/Sidebar/add-space.svg" width={16} height={16} />
+        <Image src="/images/Sidebar/question.svg" width={16} height={16} alt="question"/>
+        <Image src="/images/Sidebar/three-dots.svg" width={16} height={16}  alt="three-dots"/>
+        <Image src="/images/Sidebar/add-space.svg" width={16} height={16} alt="add-space"/>
       </div>
     </div>
   );
