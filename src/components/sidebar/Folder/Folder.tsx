604c80b9d77a0495694bbab5560b39a4c72a1bd5
eric <26416387.re@gmail.com>
2024-11-25 10:44:24 +0800

    feat/新增文件狀態

--- src/components/sidebar/Folder/Folder.tsx
+++ src/components/sidebar/Folder/Folder.tsx
@@ -36,25 +36,49 @@ const Folder = () => {
         ></Image>
       </div>
       {folderList.map(() => (
-        <div className="flex items-center  w-[12rem] h-[1.6875rem] px-[0.5rem] ">
-          <div className="flex items-center  w-[8rem] h-[1.125rem] ms-[0.375rem]">
+        <div className="flex items-center group">
+          <div className=" flex items-center justify-center h-[1.5rem] w-[1.5rem] ms-[-1rem] opacity-0 group-hover:opacity-100 transition-opacity duration-300">
             <Image
-              src="/images/Sidebar/folder-fill.svg"
-              width={16}
-              height={16}
+              className=""
+              src="/images/Sidebar/_Drag Button.svg"
+              width={12}
+              height={12}
               alt="folder"
               onClick={addFolder}
             ></Image>
-            <div className=" w-[4.8125rem] mx-[0.5rem] text-[13px] text-[#9E9E9E] rounded-[0.375rem] hover:bg-[#2C2D3C] transition-all duration-500">
-              Folder Label
+          </div>
+          <div className="flex items-center justify-between  w-[11rem] h-[1.6875rem]  rounded-[0.375rem] hover:bg-[#2C2D3C] transition-all duration-500 ">
+            <div className="flex items-center   h-[1.125rem] ps-[0.375rem] ">
+              <Image
+                src="/images/Sidebar/folder-fill.svg"
+                width={18}
+                height={18}
+                alt="folder"
+                onClick={addFolder}
+              ></Image>
+
+              <div className=" w-[4.8125rem] mx-[0.5rem] text-[13px] text-[#9E9E9E] ">
+                Folder Label
+              </div>
+              <Image
+                className="opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                src="/images/Sidebar/add-space.svg"
+                width={7}
+                height={6}
+                alt="folder"
+                onClick={addFolder}
+              ></Image>
             </div>
-            <Image
-              src="/images/Sidebar/add-space.svg"
-              width={7}
-              height={6}
-              alt="folder"
-              onClick={addFolder}
-            ></Image>
+            <div className="flex items-center justify-center h-[1.5rem] w-[1.5rem] opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <Image
+                className=""
+                src="/images/Sidebar/add-space.svg"
+                width={7}
+                height={6}
+                alt="folder"
+                onClick={addFolder}
+              ></Image>
+            </div>
           </div>
         </div>
       ))}
